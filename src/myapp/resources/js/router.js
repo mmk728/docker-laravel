b91a28e26343bd4ac7911e8fd31539b95f9fb7b0
mmk728 <inokuchi-m@arrows-systems.co.jp>
2019-10-16 17:38:50 +0900

    チュートリアル3 Vue Routerを使用できるようにした

---
+++ src/myapp/resources/js/router.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import PhotoList from './pages/PhotoList'
+import Login from './pages/Login'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        component: PhotoList
+    },
+    {
+        path: '/login',
+        component: Login
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: PhotoList
+        },
+        {
+            path: '/login',
+            component: Login
+        }
+    ]
+})
+
+export default router
