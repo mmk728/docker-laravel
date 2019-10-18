13487d3b7b336ea81c806a098b9a0a3c8d9d4a04
mmk728 <inokuchi-m@arrows-systems.co.jp>
2019-10-18 09:57:46 +0900

    ログイン状態を保持する

--- src/myapp/resources/js/app.js
+++ src/myapp/resources/js/app.js
@@ -4,6 +4,10 @@ import store from './store/index'
 import App from './App.vue'
 import './bootstrap'
 
+const createApp = async () => {
+    await store.dispatch('auth/currentUser')
+}
+
 new Vue({
     el: '#app',
     router,
@@ -11,3 +15,5 @@ new Vue({
     components: { App },
     template: '<App />'
 })
+
+createApp()
